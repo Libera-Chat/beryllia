7e2e2834fa9be2656efb79ba164a45779b70b97f
jesopo <github@lolnerd.net>
2021-09-20 01:00:34 +0000

    fix config.py's typing imports

--- beryllia/config.py
+++ beryllia/config.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from os.path     import expanduser
 from re          import compile as re_compile
-from typing      import List, Pattern, Tuple
+from typing      import Optional, Tuple
 
 import yaml
 
