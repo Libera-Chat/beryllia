6b5eb91df5eeef3042b39feca2efc4ba369cccc7
jesopo <github@lolnerd.net>
2021-11-25 20:18:16 +0000

    make `!cliconn nick foo` search nick_change table too

--- beryllia/database/db_nick_change.py
+++ beryllia/database/db_nick_change.py
@@ -1,5 +1,9 @@
+from datetime    import datetime
+from typing      import Sequence, Tuple
+
 from .common     import Table
 from ..normalise import SearchType
+from ..util      import glob_to_sql, lex_glob_pattern
 
 class NickChangeTable(Table):
     async def add(self, cliconn_id: int, nickname: str):
@@ -15,3 +19,20 @@ class NickChangeTable(Table):
 
         async with self.pool.acquire() as conn:
             await conn.execute(query, *args)
+
+    async def find_cliconn(self,
+            nickname: str
+            ) -> Sequence[Tuple[int, datetime]]:
+
+        query = """
+            SELECT cliconn.id, cliconn.ts
+                FROM cliconn
+            INNER JOIN nick_change
+                ON cliconn.id = nick_change.cliconn_id
+                AND nick_change.search_nick LIKE $1;
+        """
+
+        pattern = glob_to_sql(lex_glob_pattern(nickname))
+        param   = str(self.to_search(pattern, SearchType.NICK))
+        async with self.pool.acquire() as conn:
+            return await conn.fetch(query, param)
